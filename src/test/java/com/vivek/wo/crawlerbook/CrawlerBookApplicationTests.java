32ae0be5bd6ffc38297afd789e656b643ce068cd
vivek-wo <vivek.wei.wu@gmail.com>
2019-08-08 22:44:12 +0800

    Initial commit

---
+++ src/test/java/com/vivek/wo/crawlerbook/CrawlerBookApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vivek.wo.crawlerbook;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrawlerBookApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
